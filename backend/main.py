9882ec9e3f3ad469d4281ba83e6b638b0dad1fdf
Poojavpatel <32796407+Poojavpatel@users.noreply.github.com>
2019-06-05 00:50:24 +0530

    handeling file upload

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
-import flask
+import flask, request
 app=flask.Flask("__main__")
 
 @app.route("/" , methods = ['GET'])
 def my_index():
     return flask.render_template("index.html",token="Hello world")
+
+@app.route("/uploadfile" , methods = ['POST'])
+def myfile():
+    data=request.form['upload']
 
 app.run(debug=True)
--- backend/static/script.js
+++ backend/static/script.js
@@ -4,8 +4,8 @@ myfile.addEventListener('change',function(e){
     a=myfile.files[0];
     label.innerHTML=a.name;
     const reader= new FileReader()
-    reader.onload=function(){
-        console.log(reader.result);
-    }
+    // reader.onload=function(){
+    //     console.log(reader.result);
+    // }
     reader.readAsText(a)
 },false)
